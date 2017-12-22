c5ac585df20ffb3eb79c941637f9cf668d18234e
Yash Patel <yash.patel0419@gmail.com>
2017-12-21 18:36:05 -0600

    Added features and fixed small bugs

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ let bookmarkData;
 $.ajax({
   url: "https://bookmark-extension.herokuapp.com/getBookmarks",
   success: function(data) {
+    $("#loading").hide();
     LoadBookmarks(data);
   }
 });
@@ -30,6 +31,44 @@ function LoadBookmarks(data) {
     }
   });
 
+  function UpdateContainerHeight(){
+    if($( ".newBookmarkBar" ).is( ":visible" )||$( ".newFolderBar" ).is( ":visible" )){
+      $('.container').height(432);
+    } else {
+      $('.container').height(470);
+    }
+  }
+
+  function UpdateButtonBar(){
+    if ($(".container").prop('scrollHeight') > $(".container").height() ) {
+      if($( ".newBookmarkBar" ).is( ":visible" )||$( ".newFolderBar" ).is( ":visible" )){
+        $('.buttonBar').removeClass('BorderTop');
+        $('.newBookmarkBar').addClass('BorderTop');
+        $('.newFolderBar').addClass('BorderTop');
+      }else{
+        $('.buttonBar').addClass('BorderTop');
+      }
+    }else{
+      $('.buttonBar').removeClass('BorderTop');
+      $('.newBookmarkBar').removeClass('BorderTop');
+      $('.newFolderBar').removeClass('BorderTop');
+    }
+    
+  };
+  // function UpdateNewBar(){
+  //   if ($(".container").prop('scrollHeight') > $(".container").height() ) {
+  //     $('.buttonBar').removeClass('BorderTop');
+  //     $('.newBookmarkBar').addClass('BorderTop');
+  //     $('.newFolderBar').addClass('BorderTop');
+  //   }else{
+  //     $('.newBookmarkBar').removeClass('BorderTop');
+  //     $('.newFolderBar').removeClass('BorderTop');
+  //   }
+  // }
+  
+
+  UpdateButtonBar();
+
   $(".NewBookmark").click(function() {
     $('.newBookmarkBar').toggle();
     $('.newFolderBar').hide();
@@ -38,10 +77,15 @@ function LoadBookmarks(data) {
     }, function(tab) {
       $('.WebsiteInput').val(tab[0].title);
     });
+
+    UpdateContainerHeight();
+    UpdateButtonBar();
   });
   $(".NewFolder").click(function() {
     $('.newFolderBar').toggle();
     $('.newBookmarkBar').hide();
+    UpdateContainerHeight();
+    UpdateButtonBar();
   });
 
   $('.AddFolder').click(function() {
@@ -56,11 +100,14 @@ function LoadBookmarks(data) {
           $('.folder').last().after("<div class='folder' id='" + data.title + "'><span class='folderName'>" + data.title + "</span><span class='folderDescription w3-badge w3-small w3-green'>" + 0 + "</span><i class='deleteFolder w3-button w3-round-large w3-hover-red fa fa-times'></i></div>");
           $('.FolderSelect').append("<option>" + data.title + "</option>");
           $('.newFolderBar').hide();
+          UpdateContainerHeight();
+          UpdateButtonBar();
         } else {
           alert(data);
         }
       });
     }
+    
   });
 
   $('.AddBookmark').click(function() {
@@ -90,6 +137,8 @@ function LoadBookmarks(data) {
             $('.container').append("<div class='bookmark'><span class='bookmarkName'>" + data.title + "</span><span class='bookmarkURL'>" + data.url + "</span><i class='deleteBookmark w3-button w3-round-large w3-hover-red fa fa-times'></i></div>");
           }
           $('.newBookmarkBar').hide();
+          UpdateContainerHeight();
+          UpdateButtonBar();
         });
       };
     });
@@ -112,6 +161,7 @@ function LoadBookmarks(data) {
   $(".container").on("click", ".folder", function(e) {
     if ($(e.target).hasClass('deleteFolder')) {
       return;
+      UpdateButtonBar();
     } else {
       let that = $(this);
       $(that).css('background-color', 'white');
@@ -120,13 +170,15 @@ function LoadBookmarks(data) {
 
           $(that).css('border-left', '5px solid #4CAF50');
           $(that).css('border-right', '6px solid #4CAF50');
+          UpdateButtonBar();
         } else {
           $(that).css('border-left', '0px solid #4CAF50');
           $(that).css('border-right', '0px solid #4CAF50');
+          UpdateButtonBar();
         }
       });
+      
     }
-
   });
 
   $(".container").on("mouseenter", ".folder", function(e) {
@@ -176,7 +228,7 @@ function LoadBookmarks(data) {
           that.parent().siblings('.folderDescription').text(ItemsInFolder);
         }
         that.parent().remove();
-
+        UpdateButtonBar();
       }
     });
 
@@ -195,6 +247,7 @@ function LoadBookmarks(data) {
       data: data,
       success: function(data) {
         that.parent().remove();
+        UpdateButtonBar();
       }
     });
 
